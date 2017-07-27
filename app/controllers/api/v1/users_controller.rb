e1339c02d64520f0da05be7613514761ecc66568
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-07-27 22:35:46 +0800

    user can update date from api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::UsersController < ApplicationController
-	   def show
+  before_action :authenticate_user!, only: [:update]
+
+	  def show
       @user = User.find(params[:id])
+    end
+
+   	def update
+      @user = User.find(params[:id])
+      @user.update_attributes(update_params)
+    end
+
+    private
+
+    def update_params
+      params.require(:user).permit(:name)
     end
 
 end
